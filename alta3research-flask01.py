f1ce8fc48cd27b1311444d2a4829ca0c83441d90
skhadge <suchit.khadge@gmail.com>
2022-11-29 14:23:21 -0800

    added login page

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -1,29 +1,66 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import stats
-import os
-import jinja2
 from flask import Flask
-from flask import redirect
-from flask import url_for
+from flask import make_response
 from flask import request
 from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask import jsonify
-from flask import json
+
 
 app = Flask(__name__)
+# entry point for our users
+# login.html points to /home
+@app.route("/")
+@app.route("/logout")
+def signin():
+    return render_template("signin.html")
 
-# This is a landing point for users (a start)
-@app.route("/") # user can land at "/"
-def main():
-    return render_template("index.html")
 
-## This is where we want to redirect users for the game result data
+# set the cookie and send it back to the user
+@app.route("/home", methods = ["POST", "GET"])
+def setcookie():
+    # if user generates a POST to our API
+    if request.method == "POST":
+        if request.form.get("nm"): # if nm was assigned via the POST
+        #if request.form["nm"] <-- this also works, but returns ERROR if no nm
+            user = request.form.get("nm") # grab the value of nm from the POST
+        else: # if a user sent a post without nm then assign value defaultuser
+            user = "defaultuser"
+
+        # Note that cookies are set on response objects.
+        # Since you normally just return strings
+        # Flask will convert them into response objects for you
+        resp = make_response(render_template("index.html", userID = user))
+        # add a cookie to our response object
+                        #cookievar #value
+        resp.set_cookie("userID", user)
+
+        # return our response object includes our cookie
+        return resp
+        
+    if request.method == "GET": # if the user sends a GET
+        return redirect(url_for("index")) # redirect to index
+
+def getcookie():
+    # attempt to read the value of userID from user cookie
+    name = request.cookies.get("userID") # preferred method
+    
+    # name = request.cookies["userID"] # <-- this works but returns error
+                                       # if value userID is not in cookie
+    
+    # return HTML embedded with name (value of userID read from cookie) 
+    return render_template("index.html", userID = name)
+
+# Get the result for the particular date and render in HTML
 @app.route("/<game_date>")
 def success(game_date):
+    name = request.cookies.get("userID")
     if game_date in stats.quiz:
         #return json.dumps(stats.quiz[game_date])
-        return render_template("index.html", games = stats.quiz[game_date], game_date = game_date) 
+        return render_template("index.html", games = stats.quiz[game_date], game_date = game_date, userID = name) 
     elif game_date == "all":
         return render_template("index.html", data = stats.quiz)
     else:
@@ -45,12 +82,13 @@ def login():
             user_input = "all" # ...then no results shown
     return redirect(url_for("success", game_date = user_input)) # pass back to /success with val for name
 
-# This is a landing point for users (a start)
+#Get all data in JSON format
 @app.route("/getdata/alldata") # user can land at "/"
 def index():
     # jsonify returns legal JSON
     return jsonify(stats.quiz)
 
+#Get the game data for the particular date in JSON format
 @app.route("/getdata/<user_date>") # user can land at "/"
 def get_data(user_date):
     if user_date in stats.quiz:
@@ -60,4 +98,4 @@ def get_data(user_date):
         return "No FIFA games found. Please choose another value. "
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=2224) # runs the application
+    app.run(host="0.0.0.0", port=2224)
--- alta3research-requests02.py
+++ alta3research-requests02.py
@@ -3,7 +3,7 @@ from flask import request
 from flask import Flask
 from pprint import pprint
 
-URL= "http://127.0.0.1:2224/alldata"
+URL= "http://127.0.0.1:2224/getdata/alldata"
 URL= "http://127.0.0.1:2224/getdata/11-24-2022"
 
 resp= request.get(URL).json()
